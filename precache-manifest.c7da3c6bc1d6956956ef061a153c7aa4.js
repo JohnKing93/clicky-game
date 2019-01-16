59537f4c54a9a8ba6e72566e013fb1bb1b545f07
Johnny King <31927132+JohnKing93@users.noreply.github.com>
2019-01-16 00:06:02 -0500

    Updates

---
+++ precache-manifest.c7da3c6bc1d6956956ef061a153c7aa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "b20f65c17100e462b2e5",
+    "url": "/clicky-game/static/js/main.b20f65c1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b20f65c17100e462b2e5",
+    "url": "/clicky-game/static/css/main.f44657cb.chunk.css"
+  },
+  {
+    "revision": "976b395c2ac08071ae940febe2914938",
+    "url": "/clicky-game/index.html"
+  }
+];
